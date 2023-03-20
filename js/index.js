d54cdcb24aec83109ee6c0d052b528e96ba64a69
Mike Cuesta <cuestamichael0@gmail.com>
2023-03-20 19:03:23 -0400

    Created dir for javascript files

---
+++ js/index.js
@@ -0,0 +1,18 @@
+//A Variable lesson 
+
+let message = 'Hello.. From the past!!!'; //tore string Hello in message variable
+alert(message); //Shows value stored in Variable message as popup 
+let user = 'Mike', age = 25, greeting = 'Hello, Mike!'; //multiple vars at once
+
+message = "I've changed.."; //change value
+message = greeting; //copy value to other variable 
+alert(message);
+
+const myBirthday = '08.07.1997'; // Constant, nonchanging variable
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+//For when you have a difficult value to remeber
+let color = COLOR_ORANGE;
+alert(color);
---
+++ js/numbers.js
@@ -0,0 +1,5 @@
+const mycont = 5;
+const myFloat = 10.889;
+
+mycont;
+myFloat;
