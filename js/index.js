5092312181cd4890a496a18e0441319c441f561c
Mike Cuesta <cuestamichael0@gmail.com>
2023-03-21 20:23:50 -0400

    Practice javascript console.log()

--- js/index.js
+++ js/index.js
@@ -1,18 +1,33 @@
 //A Variable lesson 
 
-let message = 'Hello.. From the past!!!'; //tore string Hello in message variable
-alert(message); //Shows value stored in Variable message as popup 
-let user = 'Mike', age = 25, greeting = 'Hello, Mike!'; //multiple vars at once
+// let message = 'Hello.. From the past!!!'; //tore string Hello in message variable
+// alert(message); //Shows value stored in Variable message as popup 
+// let user = 'Mike', age = 25, greeting = 'Hello, Mike!'; //multiple vars at once
 
-message = "I've changed.."; //change value
-message = greeting; //copy value to other variable 
-alert(message);
+// message = "I've changed.."; //change value
+// message = greeting; //copy value to other variable 
+// alert(message);
 
-const myBirthday = '08.07.1997'; // Constant, nonchanging variable
-const COLOR_RED = "#F00";
-const COLOR_GREEN = "#0F0";
-const COLOR_BLUE = "#00F";
-const COLOR_ORANGE = "#FF7F00";
-//For when you have a difficult value to remeber
-let color = COLOR_ORANGE;
-alert(color);
+// const myBirthday = '08.07.1997'; // Constant, nonchanging variable
+// const COLOR_RED = "#F00";
+// const COLOR_GREEN = "#0F0";
+// const COLOR_BLUE = "#00F";
+// const COLOR_ORANGE = "#FF7F00";
+// //For when you have a difficult value to remeber
+// let color = COLOR_ORANGE;
+// alert(color);
+
+// console.log(2+2);
+// console.log(1+2+3+4+5+6);
+// console.log(((4+6+9)/77));
+
+// let a = 10;
+// console.log(a);
+
+// console.log(9*a);
+
+const MAX = 57;
+let actual = MAX - 13;
+let percentage = actual / MAX;
+
+console.log(percentage);
