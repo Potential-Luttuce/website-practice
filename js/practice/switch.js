2aa58a9fc9d99900222d26cd700ad72511c19f96
Mike Cuesta <cuestamichael0@gmail.com>
2023-04-02 19:03:57 -0400

    Added switch statement for printing grade out to console

--- js/practice/switch.js
+++ js/practice/switch.js
@@ -26,3 +26,31 @@
 // 	default:
 // 		console.log("Something went horribly wrong...");
 // }
+
+// // Set the student's grade
+// const grade = 87;
+
+// switch (true) {
+// 	// If score is 90 or greater
+// 	case grade >= 90:
+// 		console.log("A");
+// 		break;
+// 	// If score is 80 or greater
+// 	case grade >= 80:
+// 		console.log("B");
+// 		break;
+// 	// If score is 70 or greater
+// 	case grade >= 70:
+// 		console.log("C");
+// 		break;
+// 	// If score is 60 or greater
+// 	case grade >= 60:
+// 		console.log("D");
+// 		break;
+// 	// Anything 59 or below is failing
+// 	default:
+// 		console.log("F");
+// }
+
+// Get number corresponding to the current month, with 0 being January and 11 being December
+const month = new Date().getMonth();
