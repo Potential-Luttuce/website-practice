7abded678c0055c163dbd2fcb395d723fe53560d
Mike <Cuestamichael0@gmail.com>
2023-04-16 20:09:26 -0400

    Commit

--- DOM/exercise.js
+++ DOM/exercise.js
@@ -1,32 +1,32 @@
+//exercise 1
 const container = document.querySelector('#container');
 const red = document.createElement('p');
 red.style.color = 'red';
 red.textContent = "Hey! I'm Red!";
 container.appendChild(red);
-
+// exercise 2
 const blueText = document.createElement('h3');
 blueText.style.color = 'blue';
 blueText.textContent = "Hey! I'm a Blue h3";
 container.appendChild(blueText);
-
+// exercise 3
 const newContainer = document.createElement('div');
-
 const firstElement = document.createElement('div');
 const secondElement = document.createElement('p');
-const reset = document.createElement('button');
-
-
 newContainer.style.cssText = "border: 3px solid black; background-color: pink";
 firstElement.textContent = "Hey! I'm in a div!";
 secondElement.textContent = "Hey! Me too!";
-reset.textContent = "Hide this div";
-
-
 newContainer.appendChild(firstElement);
 newContainer.appendChild(secondElement);
-// newContainer.appendChild(reset);
-
 function show(){
     container.appendChild(newContainer);
-
 }
+
+// Events //
+//3 methods for a click
+const btn = document.querySelector('#btn'); // + js code in html code 
+btn.onclick = () => alert("Hello, World!2"); //arrow function
+const btn = dosument.querySelector('#btn');
+btn.addEventListener('click', () => {
+    alert("Hello, world3");
+})
